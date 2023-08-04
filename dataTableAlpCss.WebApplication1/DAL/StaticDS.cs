cd1d7ea0b1a2b6c3de5433676bf30fb3b6c533ac
nader <nhashemi6@gmail.com>
2023-08-04 15:54:23 +0330

    grid view + data tbl

---
+++ dataTableAlpCss.WebApplication1/DAL/StaticDS.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Presentation.DAL
+{
+    public class StaticDS
+    {
+        public System.Data.DataTable GetDT()
+        {
+            var DT = new System.Data.DataTable();
+
+            var COL = new DataColumn();
+            COL.DataType = System.Type.GetType("System.Int32");
+            COL.ColumnName = "AHP_Element_Name"; //AHP_Element_Name
+            DT.Columns.Add(COL);
+
+            var R = DT.NewRow();
+            R["AHP_Element_Name"] = 1;
+            DT.Rows.Add(R);
+
+            var R2 = DT.NewRow();
+            R2["AHP_Element_Name"] = 2;
+            DT.Rows.Add(R2);
+
+            var R3 = DT.NewRow();
+            R3["AHP_Element_Name"] = 3;
+            DT.Rows.Add(R3);
+
+            return DT;
+        }
+        public System.Data.DataTable GetDT(string colName)
+        {
+            var DT = new System.Data.DataTable();
+            
+            var COL = new DataColumn();
+            COL.DataType = System.Type.GetType("System.Int32");
+            COL.ColumnName = colName; 
+            DT.Columns.Add(COL);
+
+            var R = DT.NewRow();
+            R[colName] = 1;
+            DT.Rows.Add(R);
+
+            var R2 = DT.NewRow();
+            R[colName] = 2;
+            DT.Rows.Add(R2);
+
+            return DT;
+        }
+
+    }
+}
---
+++ dataTableAlpCss.WebApplication1/page_DT/WebForm1.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" 
+
+CodeBehind="WebForm1.aspx.cs" Inherits="dataTableAlpCss.WebApplication1.page_DT.WebForm1" %>
+
+<!DOCTYPE html>
+<link href="../Content/bootstrap-rtl.css" rel="stylesheet" />
+	<link href="../Content/mystyle.css" rel="stylesheet" />
+	<link href="../data_table/media/css/jquery.dataTables.css" rel="stylesheet" />
+	<link rel="stylesheet" type="text/css" href="../resources/syntax/shCore.css">
+	<link rel="stylesheet" type="text/css" href="../resources/demo.css">
+	<style type="text/css" class="init">
+	</style>
+	<script src="../data_table/media/js/jquery.js"></script>
+	<script src="../data_table/media/js/jquery.dataTables.js"></script>
+	<script src="../data_table/resources/syntax/shCore.js"></script>
+	<script src="../data_table/resources/demo.js"></script>
+
+	<script type="text/javascript" language="javascript" class="init">
+		$(document).ready(function () {
+            $('#GridView1').DataTable({
+				"language": {
+					"url": "../data_table/Persian.json"
+				}
+			});
+		});
+    </script>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView
+                CssClass="display"  
+                ID="GridView1" runat="server"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
